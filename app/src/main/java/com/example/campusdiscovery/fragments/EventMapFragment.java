f8bf1b0bc23aefb6c8d141a5c2cea8b627a39829
Rist, Lee Morgan <mrist6@gatech.edu>
2022-11-28 21:56:04 -0500

    update

--- app/src/main/java/com/example/campusdiscovery/fragments/EventMapFragment.java
+++ app/src/main/java/com/example/campusdiscovery/fragments/EventMapFragment.java
@@ -1,21 +1,13 @@
 package com.example.campusdiscovery.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.campusdiscovery.R;
+import androidx.fragment.app.Fragment;
 
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapController;
-import org.osmdroid.views.MapView;
-import android.app.Activity;
-import android.os.Bundle;
+import com.example.campusdiscovery.R;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,24 +18,6 @@ public class EventMapFragment extends Fragment {
 
     public EventMapFragment() {
         // Required empty public constructor
-    }
-
-    public class OsmdroidDemoMap extends Activity {
-        private MapView         mMapView;
-        private MapController   mMapController;
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.osm_main);
-            mMapView = (MapView) findViewById(R.id.mapview);
-            mMapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
-            mMapView.setBuiltInZoomControls(true);
-            mMapController = (MapController) mMapView.getController();
-            mMapController.setZoom(13);
-            GeoPoint gPt = new GeoPoint(51500000, -150000);
-            mMapController.setCenter(gPt);
-        }
     }
 
     /**
