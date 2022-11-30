ab47f5af298133d4e98b255bb3614592b1f8e418
Rist, Lee Morgan <mrist6@gatech.edu>
2022-11-29 20:36:10 -0500

    trying to get map to work

--- app/src/main/java/com/example/campusdiscovery/fragments/EventMapFragment.java
+++ app/src/main/java/com/example/campusdiscovery/fragments/EventMapFragment.java
@@ -1,59 +1,55 @@
 package com.example.campusdiscovery.fragments;
 
-import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.campusdiscovery.R;
 
 import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapListener;
+import org.osmdroid.events.ScrollEvent;
+import org.osmdroid.events.ZoomEvent;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.mylocation.DirectedLocationOverlay;
 
 //osmdroid is in the C:\...\GitHub\S1\app\src\main\res
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link EventMapFragment#newInstance} factory method to
+ * Use the {@link EventMapFragment} factory method to
  * create an instance of this fragment.
  */
-public class EventMapFragment extends Fragment {
+public class EventMapFragment extends Fragment implements LocationListener {
+
+    private static EventMapFragment INSTANCE = null;
+    View view;
+
+    MapView map = null;
+    private MapView osm;
+    private MapController mc;
+    private LocationManager locationManager;
+    private CompassOverlay compassOverlay;
+    private DirectedLocationOverlay locationOverlay;
 
     public EventMapFragment() {
         // Required empty public constructor
-    }
-
-    MapView map = null;
-    @Override public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        //load/initialize the osmdroid configuration, this can be done
-        Activity act = getActivity();
-        Configuration.getInstance().load(act, PreferenceManager.getDefaultSharedPreferences(act));
-
-    }
-
-    public void onResume(){
-        super.onResume();
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
-    }
-
-    public void onPause(){
-        super.onPause();
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().save(this, prefs);
-        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
 
     /**
@@ -62,11 +58,18 @@ public class EventMapFragment extends Fragment {
      *
      * @return A new instance of fragment EventMapFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static EventMapFragment newInstance() {
-        EventMapFragment fragment = new EventMapFragment();
-        return fragment;
+    public static EventMapFragment getINSTANCE() {
+        if (INSTANCE == null)
+            INSTANCE = new EventMapFragment();
+        return INSTANCE;
+
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
 
 //    @Override
 //    public void onCreate(Bundle savedInstanceState) {
@@ -77,12 +80,71 @@ public class EventMapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         //set layout
-        View view = inflater.inflate(R.layout.fragment_event_map, container, false);
+        view = inflater.inflate(R.layout.fragment_event_map, container, false);
+
+        //load/initialize the osmdroid configuration, this can be done
+        Context ctx = getActivity().getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        MapView osm = view.findViewById(R.id.map);
+        osm.setTileSource(TileSourceFactory.MAPNIK);
+        osm.setBuiltInZoomControls(true);
+        osm.setMultiTouchControls(true);
+
+        mc = (MapController) osm.getController();
+        mc.setZoom(15);
+
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, this);
 
         // Inflate the layout for this fragment
         //inflate and create the map
-        map = (MapView) getView().findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.MAPNIK);
+        osm.setMapListener(new MapListener() {
+            @Override
+            public boolean onScroll(ScrollEvent event) {
+                Log.i("Script", "onScroll()");
+                return false;
+            }
+
+            @Override
+            public boolean onZoom(ZoomEvent event) {
+                Log.i("Script", "onZoom()");
+                return false;
+            }
+        });
         return view;
+    }
+
+    public void addMarker (GeoPoint center){
+        Marker marker = new Marker(osm);
+        marker.setPosition(center);
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        marker.setIcon(getResources().getDrawable(R.drawable.loc));
+        osm.getOverlays().clear();
+        osm.getOverlays().add(marker);
+        osm.invalidate();
+        marker.setTitle("Sua Localização");
+    }
+
+    public void onResume(){
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map.onResume();
+    }
+
+    public void onPause(){
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map.onPause();  //needed for compass
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
     }
 }
